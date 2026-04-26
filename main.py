0cd21615b8a5a8cf1415017f5c4e28507625c25f
V1ltrumite <viltrumite619@gmail.com>
2026-04-26 15:14:33 -0700

    Created a clock object to track time and capped FPS at 60

--- main.py
+++ main.py
@@ -6,9 +6,8 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting Asteroids with pygame version: 2.6.1")
-    print("Screen width: 1280")
-    print("Screen height: 720")
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         log_state()
         for event in pygame.event.get():
@@ -16,6 +15,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
         
 
 if __name__ == "__main__":
