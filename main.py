e123c0482e0ea1104bb45744a2086dafd3999bde
V1ltrumite <viltrumite619@gmail.com>
2026-04-25 23:33:16 -0700

    Update main.py with screen function and while loop.

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
 
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids with pygame version: 2.6.1")
     print("Screen width: 1280")
     print("Screen height: 720")
+    while True:
+        
 
 if __name__ == "__main__":
     main()
