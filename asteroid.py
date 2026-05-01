cc60104248d0e5ed92523247d8a52364f2037d93
V1ltrumite <viltrumite619@gmail.com>
2026-05-01 16:07:29 -0700

    Added kill method to asteroid.py and then checked for if it was a minimum size. Added .split() method for asteroids larger than minimum size. Replaced asteroid kill method with asteroid split method in main game loop.

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
 import pygame
+import random
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from logger import log_event
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
 
 
 class Asteroid(CircleShape):
@@ -14,3 +16,18 @@ class Asteroid(CircleShape):
     
     def update(self, dt):
         self.position += self.velocity * dt
+
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        log_event("asteroid_split")
+        angle = random.uniform(20, 50)
+        new_velocity1 = self.velocity.rotate(angle)
+        new_velocity2 = self.velocity.rotate(-angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid1.velocity = new_velocity1 * 1.2
+        asteroid2.velocity = new_velocity2 * 1.2
--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def main():
            for shot in shots:
                if shot.collides_with(asteroid):
                    log_event("asteroid_shot")
-                   asteroid.kill()
+                   asteroid.split()
                    shot.kill()
         for obj in drawable:
             obj.draw(screen)
